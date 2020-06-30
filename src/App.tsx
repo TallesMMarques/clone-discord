92ef06f1607d380397e044cc4a3400d41d912346
Talles Marques <tallesemanuel1@gmail.com>
2020-06-30 00:13:19 -0300

    Adicionando React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World2</h1>
+    </>
+  );
+}
+
+export default App;
